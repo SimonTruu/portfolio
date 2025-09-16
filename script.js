f16ac0fa02c3d6cb650ae84c974c037fffc37bb0
SimonTruu <simon.truuberg@elev.ga.lbs.se>
2025-09-16 10:58:48 +0200

    gy

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const startMenu = document.getElementById("startMenu")
+const content = document.querySelectorAll(".content")
+
 document.querySelectorAll("#startMenu .menuItem").forEach(item => {
     item.addEventListener("click", () => {
+      sectionID = `${item.id}Section`
+      section = document.getElementById(sectionID)
+
+      startMenu.classList.add("hidden")
+      content.forEach(section => section.classList.add("hidden"))
+
+      section.classList.remove("hidden")
+      
+/*
       const file = `${item.id}.html`;
       window.location.href = file;
+*/
     });
 });
+
+function toggleLight(){
+  document.body.classList.toggle("light-mode");
+}
+
