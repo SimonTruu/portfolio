27ec8c2fed78ed88ccf32df210c319abbd90d896
SimonTruu <simon.truuberg@elev.ga.lbs.se>
2025-10-07 09:31:13 +0200

    git

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+
 const startMenu = document.getElementById("startMenu")
 const content = document.querySelectorAll(".content")
 
@@ -6,19 +8,31 @@ document.querySelectorAll("#startMenu .menuItem").forEach(item => {
       sectionID = `${item.id}Section`
       section = document.getElementById(sectionID)
 
-      startMenu.classList.add("hidden")
-      content.forEach(section => section.classList.add("hidden"))
+      startMenu.style.display = "none"
+      content.forEach(section => section.style.display = "none")
 
-      section.classList.remove("hidden")
-      
-/*
-      const file = `${item.id}.html`;
-      window.location.href = file;
-*/
+      section.style.display = "flex"
     });
 });
 
+document.querySelectorAll(".back").forEach( item => {
+  item.addEventListener("click", () => {
+    content.forEach(section => section.style.display = "none")
+    startMenu.style.display = "flex"
+  })
+})
+
+document.getElementById("styleToggel").addEventListener("click", toggleLight)
+
 function toggleLight(){
   document.body.classList.toggle("light-mode");
+
+  if (document.body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
 }
 
+if (localStorage.getItem("theme") === "light") {document.body.classList.add("light-mode");}
+});
